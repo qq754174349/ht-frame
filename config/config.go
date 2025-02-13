bc1c0f9f3bba2020e710c7ddcd7313d53e4502a3
lihaitao <lihaitao@cnhnkj.com>
2025-02-13 10:42:31 +0800

    5444

--- config/config.go
+++ config/config.go
@@ -1,5 +1,42 @@
 package config
 
+type AppConfig struct {
+	Active     string
+	AppName    string `yaml:"app_name" json:"app_name" mapstructure:"app_name"`
+	Web        WebConfig
+	Log        LogConfig
+	Datasource datasource
+}
+
+type LogConfig struct {
+	Level       string
+	OutputPaths string `json:"output_paths" yaml:"output_paths" mapstructure:"output_paths"`
+}
+
+type WebConfig struct {
+	Port string
+}
+
+type datasource struct {
+	Mysql MysqlConfig
+	Redis RedisConfig
+}
+
+type MysqlConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+}
+
+type RedisConfig struct {
+	Addr     string
+	User     string
+	Password string
+	DB       int
+}
+
 type Configuration interface {
 	Init(config interface{}) error
 }
